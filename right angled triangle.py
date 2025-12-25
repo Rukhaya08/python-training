2b3e97a33397f0313053b665f1f96d10731ef108
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-25 20:41:36 +0530

    Update right angled triangle.py

--- right angled triangle.py
+++ right angled triangle.py
@@ -1,5 +1,19 @@
+version-1
 x=int(input())
 for i in range(x):
     for j in range(i+1):
         print("*",end=" ")
     print()
+
+version-2 (using numbers)
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print()
+
+version-3 
+x=int(input())
+for i in range(1,x+1):
+    print("*"*i)
+
