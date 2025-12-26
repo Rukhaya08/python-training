798cc2c3ad2959f9d370530efcb976877a798643
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-26 21:50:26 +0530

    Update leftover string.py

--- leftover string.py
+++ leftover string.py
@@ -1,3 +1,10 @@
+'''Q)You are given two strings A and B.
+Your task is to find and return a string representing the leftover string in A after removing all the letters that exist in string B.
+Return "empty" if the output does not contain any value.
+Note:
+1)strings A and B contains alphabets in upper case only.
+2)A single alphabet in B can replace all the occurence of that alphabet in A.  '''
+
 def leftover(a,b):
     left=""
     remove=set(b)
