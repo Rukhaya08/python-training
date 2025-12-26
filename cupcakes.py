4be0b7fdb576da02cb31ccc21edc494c949afc14
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-26 21:32:42 +0530

    Update cupcakes.py

--- cupcakes.py
+++ cupcakes.py
@@ -1,3 +1,7 @@
+''' Q)You are given an array A of size N,where each element represents the number of cupcakes sold in a single transaction.
+Your task is to find and return an integer value representing the sum of cupcakes from the transactions where 5 or more cupcakes were sold.
+Return 0 if there is no transaction with more than 5 cupcakes sold.            '''
+    
 def cupcakes(n,arr):
     sum=0
     for i in range(n):
