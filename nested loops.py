ba14abb1eb1ef4d483873d4bd428f5cf9d173c14
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-26 14:43:34 +0530

    Update nested loops.py

--- nested loops.py
+++ nested loops.py
@@ -1,4 +1,15 @@
+using for:
 for r in range(4):
     for c in range(5):
         print("r=",r,"c=",c)
     print()
+
+using while:
+r=0
+while r<4:
+    c=0
+    while c<5:
+        print(r,c)
+        c+=1
+    print()
+    r+=1
