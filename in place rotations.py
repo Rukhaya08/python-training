42f918897ba6682606d880be61a5730ec9f8110b
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-27 11:42:27 +0530

    Update in place rotations.py

--- in place rotations.py
+++ in place rotations.py
@@ -1,3 +1,4 @@
+#reverse the array
 x=[1,2,3,4,5]
 l=0
 r=len(x)-1
@@ -6,3 +7,17 @@ while l<r:
     l+=1
     r-=1
 print(x)
+
+#leetcode moveZeros-283(not used in leetcode)
+def moveZeros(nums):
+    x=[]
+    y=[]
+    for i in range(len(nums)):
+        if nums[i]!=0:
+            x.append(nums[i])
+        else:
+            y.append(nums[i])
+    z=x+y
+    print(z)
+nums=[0,1,0,3,12]
+moveZeros(nums)
