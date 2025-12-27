38794850227cf36b9a463ff9c69d3a190b4272ee
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-27 20:46:04 +0530

    Update Leetcode problem-283(movezeros).py

--- Leetcode problem-283(movezeros).py
+++ Leetcode problem-283(movezeros).py
@@ -1,3 +1,4 @@
+#Leetcode version
 def moveZeroes(nums):
         idx=0
         for i in range(len(nums)):
@@ -7,4 +8,20 @@ def moveZeroes(nums):
         for i in range(idx,len(nums)):
             nums[i]=0
         return nums
+
 print(moveZeroes([0,1,0,3,12]))
+
+
+#leetcode moveZeros-283(not used in leetcode)
+def moveZeros(nums):
+    x=[]
+    y=[]
+    for i in range(len(nums)):
+        if nums[i]!=0:
+            x.append(nums[i])
+        else:
+            y.append(nums[i])
+    z=x+y
+    print(z)
+nums=[0,1,0,3,12]
+moveZeros(nums)
