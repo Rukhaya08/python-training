3c86d27240b69a0d6a4bc469777eaa43bf8ba048
Rukhaya08 <shaikrukhaya2005@gmail.com>
2025-12-29 10:13:06 +0530

    Update leetcode problem-169(majority element).py

--- leetcode problem-169(majority element).py
+++ leetcode problem-169(majority element).py
@@ -3,3 +3,19 @@ def majorityElement(nums):
     return nums[len(nums)//2]
 nums=list(map(int,input().split()))
 print(majorityElement(nums))
+
+#without builtin function sort
+def majorityElement(nums):
+        count=0
+        candidate=0
+        for num in nums:
+            if count==0:
+                candidate=num
+                count+=1
+            elif candidate==num:
+                count+=1
+            else:
+                count-=1
+        return candidate
+nums=list(map(int,input().split()))
+print(majorityElement(nums))
